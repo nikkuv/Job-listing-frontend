d7ce8f1759894a149a86c4fa26017c2e8ddf1d5e
saumya verma <2000vermasaumya18@gmail.com>
2021-11-11 21:46:02 +0530

    fix header

--- job-listing/src/components/Header.js
+++ job-listing/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () =>{
     return (
-        <div className='w-full mb-6 container relative z-1 bg-cyan'>
+        <div className='w-full mb-6 relative z-1 bg-cyan'>
             <img className='w-full  md:hidden' src='/images/bg-header-mobile.svg' alt='backgroundImage'></img>
             <img className='w-full hidden md:block' src ='/images/bg-header-desktop.svg' alt='backgroundImage'></img>
         </div>
